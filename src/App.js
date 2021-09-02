198f74ee12ca2864ff9858ed50f0d96d26fa24fe
RaphaAmericano <raphael@raphaelamericano.com.br>
2021-09-02 20:37:08 -0300

    Adicionando homepage ao package.json

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            React Redux
+            React Redux - Explorers of Ixalan
           </a>
         </span>
       </header>
